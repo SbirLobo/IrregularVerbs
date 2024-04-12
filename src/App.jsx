8e4350d6b2f682b0f2f64dedc43ebff1003f1865
Sébastien Birolleau <sebastien.birolleau@gmail.com>
2024-04-12 12:10:47 +0200

    decrease the length of the list with each new verb

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,34 @@ import verb60 from "./data/verb60";
 import verb30 from "./data/verb30";
 
 function App() {
-  const [switchCorrection, setSwitchCorrection] = useState(0);
+  // data : all verbs
+  const [data, setData] = useState([verb30, verb60, verb90, verb176]);
+  // correctionDisplay (0: start, 1: translation, 2: correction) = display of the correction
+  const [correctionDisplay, setCorrectionDisplay] = useState(0);
+  //selectedVerbIndex = index of the verb chosen by chance
   const [selectedVerbIndex, setSelectedVerbIndex] = useState(-1);
-  const [data] = useState([verb30, verb60, verb90, verb176]);
+  // dataIndex = index of the data list
   const [dataIndex, setDataIndex] = useState(0);
-  const [dataName, setDataName] = useState("30 verbs");
+  // current data list of verbs : array
   const [currentList, setCurrentList] = useState(data[0]);
+  // current verb : object
+  const [currentVerb, setCurrentVerb] = useState({});
+
+  function newVerbSelection() {
+    const tempsSelectedVerbIndex = Math.floor(
+      Math.random() * currentList.length
+    );
+    const tempCurrentList = currentList;
+    // const tempCurrentVerb = tempCurrentList.splice(tempsSelectedVerbIndex, 1);
+    const tempCurrentVerb = tempCurrentList[tempsSelectedVerbIndex];
+    const NewCurrentList = tempCurrentList.filter(
+      (i) => i.base_form !== tempCurrentVerb.base_form
+    );
+    setCurrentVerb(tempCurrentVerb[0]);
+    setCurrentVerb(tempCurrentVerb);
+    setCurrentList(NewCurrentList);
+    setSelectedVerbIndex(tempsSelectedVerbIndex);
+  }
 
   function listHandleClick() {
     if (dataIndex === data.length - 1) {
@@ -20,31 +42,26 @@ function App() {
       setDataIndex(dataIndex + 1);
     }
     setSelectedVerbIndex(-1);
+    setCurrentList(data[dataIndex]);
+    setCorrectionDisplay(0);
+    setData([verb30, verb60, verb90, verb176]);
   }
 
   function goHandleClick() {
-    if (switchCorrection === 0) {
-      setSelectedVerbIndex(Math.floor(Math.random() * data[dataIndex].length));
+    if (correctionDisplay === 0) {
+      newVerbSelection();
     }
-    if (switchCorrection != 2) {
-      setSwitchCorrection(switchCorrection + 1);
+    if (correctionDisplay === 2) {
+      setCorrectionDisplay(1);
+      newVerbSelection();
     } else {
-      setSwitchCorrection(1);
-      setSelectedVerbIndex(Math.floor(Math.random() * data[dataIndex].length));
+      setCorrectionDisplay(correctionDisplay + 1);
     }
   }
 
   useEffect(() => {
-    if (dataIndex === 0) {
-      setDataName("30 verbs");
-    } else if (dataIndex === 1) {
-      setDataName("60 verbs");
-    } else if (dataIndex === 2) {
-      setDataName("90 verbs");
-    } else if (dataIndex === 3) {
-      setDataName("176 verbs");
-    }
     setCurrentList(data[dataIndex]);
+    setCorrectionDisplay(0);
   }, [dataIndex, data]);
 
   return (
@@ -52,27 +69,38 @@ function App() {
       <div className="flex flex-col"></div>
       <h1 className="text-3xl font-bold p-4">Irregular Verbs</h1>
       <div className="flex flex-row justify-around">
-        <button onClick={listHandleClick} className="text-xl my-8">
-          {dataName}
-        </button>
+        {currentList.length === 176 ? (
+          <button onClick={listHandleClick} className="text-xl my-8 w-72">
+            All of the {currentList.length} verbs
+          </button>
+        ) : (
+          <button onClick={listHandleClick} className="text-xl my-8 w-72">
+            {currentList.length} most common verbs
+          </button>
+        )}
       </div>
       <div className="flex flex-col">
-        <button onClick={goHandleClick} className="text-xl my-8">
-          GO
-        </button>
+        {currentList.length === 0 && correctionDisplay === 2 ? (
+          <button className="text-xl my-8 text-pink-600 font-bold">
+            GOOD GAME
+          </button>
+        ) : (
+          <button onClick={goHandleClick} className="text-xl my-8">
+            GO
+          </button>
+        )}
+
         {selectedVerbIndex !== -1 && (
           <p className="text-xl my-8 text-yellow-300">
-            {currentList[selectedVerbIndex].french_translation}
+            {currentVerb.french_translation}
           </p>
         )}
       </div>
-      {selectedVerbIndex !== -1 && switchCorrection === 2 && (
+      {selectedVerbIndex !== -1 && correctionDisplay === 2 && (
         <div className="flex flex-col MY-8">
-          <p className="text-xl">{currentList[selectedVerbIndex].base_form}</p>
-          <p className="text-xl">{currentList[selectedVerbIndex].past_tense}</p>
-          <p className="text-xl">
-            {currentList[selectedVerbIndex].past_participle}
-          </p>
+          <p className="text-xl">{currentVerb.base_form}</p>
+          <p className="text-xl">{currentVerb.past_tense}</p>
+          <p className="text-xl">{currentVerb.past_participle}</p>
         </div>
       )}
     </>
